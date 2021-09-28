10d990cb11c2ee8bbdb6c043af6f3a05c4a9eee0
Stephen Pearce <stephen.r.pearce@gmail.com>
2021-09-27 20:24:48 -0400

    Add websockets check to avoid breaking SSR, added unmount logic to close and kill websocket instance

--- src/useWebsocket.js
+++ src/useWebsocket.js
@@ -1,9 +1,11 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 export default (url) => {
   const ws = useRef(null);
   const [messageCount, setMessageCount] = useState(0);
   const [received, setReceived] = useState(null);
+
+  if (typeof WebSocket === 'undefined') return [() => {}, received, messageCount];
 
   if (!ws.current) {
     ws.current = new WebSocket(url);
@@ -14,6 +16,13 @@ export default (url) => {
   }
 
   const send = (message) => ws.current.send(message);
+
+  useEffect(() => () => {
+    if (!ws.current) return;
+
+    ws.current.close();
+    ws.current = null;
+  }, []);
 
   return [send, received, messageCount, ws.current];
 };
