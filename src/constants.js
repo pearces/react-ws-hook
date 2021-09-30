59f274a47d94c53b5e4981e806ebae6ace114728
Stephen Pearce <stephen.r.pearce@gmail.com>
2021-09-29 22:17:31 -0400

    Added warning when WebSocket is unsupported (i.e. SSR rendering), limited exposed WebSocket properties that are passed from hook, bumped several packages to latest version

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const READY_STATES = ['CONNECTING', 'OPEN', 'CLOSING', 'CLOSED'];
+
+export const ERRORS = {
+  WS_UNSUPPORTED: 'Global WebSocket object not supported or unavailable, not running useWebsocket hook'
+};
--- src/useWebsocket.js
+++ src/useWebsocket.js
@@ -1,11 +1,18 @@
+/* eslint-disable no-console */
 import { useRef, useState, useEffect } from 'react';
+import { READY_STATES, ERRORS } from './constants';
+
+const { WS_SUPPORTED } = ERRORS;
 
 export default (url) => {
   const ws = useRef(null);
   const [messageCount, setMessageCount] = useState(0);
   const [received, setReceived] = useState(null);
 
-  if (typeof WebSocket === 'undefined') return [() => {}, received, messageCount];
+  if (typeof WebSocket === 'undefined') {
+    console.warn(WS_SUPPORTED);
+    return [() => {}, received, messageCount, { readyState: READY_STATES[0] }];
+  }
 
   if (!ws.current) {
     ws.current = new WebSocket(url);
@@ -24,5 +31,6 @@ export default (url) => {
     ws.current = null;
   }, []);
 
-  return [send, received, messageCount, ws.current];
+  const { readyState, url: wsUrl } = ws.current;
+  return [send, received, messageCount, { readyState: READY_STATES[readyState], url: wsUrl }];
 };
