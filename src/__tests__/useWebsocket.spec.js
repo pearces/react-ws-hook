ceff094bd2cae59abfdf2e4ff98992290f4b7de9
Stephen Pearce <stephen.r.pearce@gmail.com>
2022-05-03 22:09:37 -0400

    chore(tests): refactored unit tests to avoid duplication

--- src/__tests__/useWebsocket.spec.js
+++ src/__tests__/useWebsocket.spec.js
@@ -1,7 +1,7 @@
 import { renderHook } from '@testing-library/react-hooks';
 import net from 'net';
 import { WebSocketServer } from 'ws';
-import useWebsocket from '../useWebsocket';
+import useWebsocket from '..';
 import { CONNECTION_STATES } from '../constants';
 
 const { CONNECTING } = CONNECTION_STATES;
@@ -23,25 +23,42 @@ beforeAll(async () => {
   testUrl = `ws://localhost:${port}/`;
 });
 
+const logger = {
+  error: jest.fn(),
+  warn: jest.fn()
+};
+
+let wss;
+let error;
+
+const onError = jest.fn((err) => { error = err; });
+const onConnect = jest.fn();
+const onOpen = jest.fn();
+
+const startServer = () => {
+  wss = new WebSocketServer({ port });
+  wss.on('connection', onConnect);
+};
+
+const defaultOptions = { onError, logger, onOpen };
+
+afterEach(() => {
+  error = undefined;
+
+  onError.mockReset();
+  onConnect.mockReset();
+  onOpen.mockReset();
+
+  logger.error.mockReset();
+  logger.warn.mockReset();
+
+  if (wss) wss.close();
+});
+
 describe('invocation', () => {
-  const logger = {
-    error: jest.fn(),
-    warn: jest.fn()
-  };
-
-  let error;
-  const onError = jest.fn((err) => { error = err; });
-
-  afterEach(() => {
-    error = undefined;
-    onError.mockReset();
-    logger.error.mockReset();
-    logger.warn.mockReset();
-  });
-
   it('fails when unable to connect with basic options', async () => {
     const { result, waitForNextUpdate } = renderHook(
-      () => useWebsocket(testUrl, { onError, logger })
+      () => useWebsocket(testUrl, defaultOptions)
     );
 
     const [,, { readyState, url }] = result.current;
@@ -55,18 +72,12 @@ describe('invocation', () => {
   });
 
   it('connects with basic options', async () => {
-    const onConnect = jest.fn();
-    const onOpen = jest.fn();
-    const wss = new WebSocketServer({ port });
+    startServer();
 
-    wss.on('connection', onConnect);
-
-    const { result, waitForNextUpdate } = renderHook(
-      () => useWebsocket(testUrl, { onError, logger, onOpen })
+    const { waitForNextUpdate } = renderHook(
+      () => useWebsocket(testUrl, defaultOptions)
     );
-    const [,, { url }] = result.current;
 
-    expect(url).toEqual(testUrl);
     await waitForNextUpdate();
 
     expect(error).toBeUndefined();
@@ -75,7 +86,5 @@ describe('invocation', () => {
 
     expect(onConnect).toHaveBeenCalled();
     expect(onOpen).toHaveBeenCalled();
-
-    wss.close();
   });
 });
